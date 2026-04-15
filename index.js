73855075dbc110e261caad88ce9d58270684c1c7
stephanieysh <stephanieshinhui@gmail.com>
2026-04-16 02:02:06 +0800

    Add database integration

--- index.js
+++ index.js
@@ -2,45 +2,71 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
+app.use(express.json()); //JSON API
+
 const PORT = process.env.PORT;
 const path = require('path');
 
+// Middleware to parse URL-encoded bodies
 app.use(express.urlencoded({ extended: true }));
+
+// Import the database connection pool
+const itemsPool = require('./dbconfig');
+
+// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-const tasks = [
-        {
-            id:1,
-            task: "Buy groceries",
-            completed: false
-        },
-        {
-            id:2,
-            task: "Read a book",
-            completed: false
-        }
-    ];
 app.set('view engine', 'ejs');
 
 app.set('views', './views');
 
-app.get('/', (req, res) => {
-    res.render('tasks', { tasks });
-});
+// Initialize the database and create the tasks table if it doesn't exist
+async function initDB() {
+    try {
+        await itemsPool.query(`
+            CREATE TABLE IF NOT EXISTS tasks (
+                id SERIAL PRIMARY KEY,
+                task TEXT,
+                completed BOOLEAN DEFAULT false
+            );
+        `);
+        console.log("Table ready");
+    } catch (err) {
+        console.error("Error creating table:", err);
+    }
+}
+
+initDB();
 
 app.listen(PORT,() => {
-    console.log(`Server is running on port http://localhost:${PORT}`);
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
 
-app.post('/tasks/:id', (req, res) => {
-    const taskId = parseInt(req.params.id);
-    const taskIndex = tasks.findIndex(task => task.id === taskId);
-    if (taskIndex !== -1) {
-        tasks[taskIndex].completed = !tasks[taskIndex].completed;
-        console.log(`Task with ID ${taskId} has been updated.`);
-    } else {
-        return res.status(404).send('Task not found');
+app.post('/tasks', async(req, res) => {
+    try{
+        const newTask = await itemsPool.query(
+            'INSERT INTO tasks (task) VALUES ($1) RETURNING *',
+            [req.body.task]
+        );
+        res.redirect('/'); // Redirect to the main page after adding a task
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error.message)
     }
-    console.log(res.body);
-    res.redirect('/');
+});
+
+app.post('/tasks/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        await itemsPool.query(
+            'DELETE FROM tasks WHERE id = $1',
+            [id]
+        );
+
+        res.redirect('/');
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error.message);
+    }
 });
